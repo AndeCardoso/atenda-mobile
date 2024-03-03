898a013cad0c4de5a7412b680a2b2e4f9b95aa3f
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-03-03 15:31:13 -0300

    feat: Add global colors and theme structure

---
+++ src/app/global/styles/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  PRIMARY: "#60CCA7",
+  SECONDARY: "#293148",
+
+  PRIMARY_INACTIVE: "#5A8476",
+  SECONDARY_INACTIVE: "#5A627A",
+
+  WARNING: "#EB654B",
+
+  BLACK: "#000000",
+  WHITE: "#FFFFFF",
+};
