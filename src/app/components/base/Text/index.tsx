3a9e47692b15ab998179e0c653ec6c199608e515
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-03-19 20:22:30 -0300

    update: Changing logo brand and colors of login page

--- src/app/components/base/Text/index.tsx
+++ src/app/components/base/Text/index.tsx
@@ -2,7 +2,6 @@ import React, { PropsWithChildren } from "react";
 import { StyleProp, TextStyle } from "react-native";
 import { Text as PaperText } from "react-native-paper";
 import { colors } from "@global/styles/colors";
-import { useFonts } from "expo-font";
 import { useTheme } from "styled-components";
 
 export type Colors = keyof typeof colors;
@@ -24,13 +23,6 @@ export const Text = ({
   style,
 }: ITextProps) => {
   const { colors } = useTheme();
-  const [fontsLoaded, fontError] = useFonts({
-    Anton: require("@fonts/Anton-Regular.ttf"),
-  });
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
 
   const fontStyle: StyleProp<TextStyle> = {
     fontSize: size,
