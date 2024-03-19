cab680ee3e40075c8d5cdc5d9d6f5907f494c53f
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-03-19 20:25:56 -0300

    refact: Remove unnecessary requisition

--- src/app/hooks/connection/useUser.ts
+++ src/app/hooks/connection/useUser.ts
@@ -1,35 +1,13 @@
 import { HttpStatusCode } from "axios";
 import { useMutation } from "react-query";
-import { useNavigation } from "@react-navigation/native";
 import { SuperConsole } from "@tools/indentedConsole";
 import UserService from "@services/user";
-import { UserRegisterRequestDTO } from "@services/user/dtos/UserRegisterRequestDTO";
+import { UserRegisterRequestDTO } from "@services/user/dtos/request/UserRegisterRequestDTO";
 
 export const useUser = () => {
   const userService = new UserService();
 
   const register = useMutation(
-    async ({ name, email, password }: UserRegisterRequestDTO) =>
-      await userService.register({ name, email, password }),
-    {
-      onSuccess: async ({ statusCode, body }) => {
-        switch (statusCode) {
-          case HttpStatusCode.Ok:
-            return body;
-          case HttpStatusCode.BadRequest:
-          default:
-            SuperConsole(body);
-            return;
-        }
-      },
-      onError: async (error) => {
-        console.log("aqui", JSON.stringify(error, null, 2));
-        return;
-      },
-    }
-  );
-
-  const list = useMutation(
     async ({ name, email, password }: UserRegisterRequestDTO) =>
       await userService.register({ name, email, password }),
     {
