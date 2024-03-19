1784b852b7f423b3f84b05ccadecae0f5ee63ef5
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-03-19 20:24:04 -0300

    fix: Add icon name prop with types

--- src/app/components/base/Icon/index.tsx
+++ src/app/components/base/Icon/index.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { TIconNames } from "./types";
 
 interface IIconProps {
-  name: string;
+  name: TIconNames;
   size?: number;
   color?: string;
 }
 
 export const Icon = ({ name, color, size }: IIconProps) => {
-  return <MaterialCommunityIcons name={name} size={size} color={color} />;
+  return (
+    <MaterialCommunityIcons
+      name={name}
+      size={size}
+      color={color}
+      style={{ fontFamily: MaterialCommunityIcons.getFontFamily() }}
+    />
+  );
 };
