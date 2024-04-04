fc905a75e0ee3925a31c57330528694a24cec8f7
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-04-03 21:18:50 -0300

    fix: Add req reload when page is foccused

--- src/app/pages/signedIn/technicians/useTechniciansController.ts
+++ src/app/pages/signedIn/technicians/useTechniciansController.ts
@@ -1,38 +1,38 @@
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { useTheme } from "styled-components";
 import { SignedInScreens } from "@routes/screens";
-import { useInfiniteQuery } from "react-query";
+import { useInfiniteQuery, useQueryClient } from "react-query";
 import TechnicianService from "@services/technician";
 import { HttpStatusCode } from "axios";
 import { SuperConsole } from "@tools/indentedConsole";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export const useTechniciansController = () => {
   const { colors } = useTheme();
   const { navigate, canGoBack, goBack } = useNavigation<any>();
+  const queryClient = useQueryClient();
+
   const technicianService = new TechnicianService();
 
   const [technicianSearch, setTechnicianSearch] = useState("");
 
-  const { data } = useInfiniteQuery(
+  const { data, isLoading } = useInfiniteQuery(
     ["technicians", technicianSearch],
     async ({ pageParam }) => {
-      const response = await technicianService.list({
+      const { statusCode, body } = await technicianService.list({
         limit: 10,
         page: pageParam ?? 1,
         column: "name",
         order: "asc",
         search: undefined,
       });
-      switch (response.statusCode) {
+      switch (statusCode) {
         case HttpStatusCode.Ok:
-          return response.body;
+          return body;
         case HttpStatusCode.NoContent:
         case HttpStatusCode.BadRequest:
-          SuperConsole(response.body);
-          return;
         default:
-          SuperConsole(response.body);
+          SuperConsole(body);
           return;
       }
     },
@@ -47,7 +47,6 @@ export const useTechniciansController = () => {
         console.log("error - technicians", JSON.stringify(error, null, 2));
         return;
       },
-      refetchOnWindowFocus: true,
     }
   );
 
@@ -92,11 +91,20 @@ export const useTechniciansController = () => {
     },
   ];
 
+  useFocusEffect(
+    useCallback(() => {
+      queryClient.invalidateQueries("technicians");
+    }, [])
+  );
+
   return {
     technicianList: data?.pages[0]?.data,
     onTechnicianSearch,
     handleGoToDetails,
     handleGoBack,
     fabActions,
+    viewState: {
+      loading: isLoading,
+    },
   };
 };
