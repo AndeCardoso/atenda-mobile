65f6474075749f2a7266bc39cc010a9bf4bfd634
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-04-06 22:07:46 -0300

    refact: Change card base component to touchble opacity

--- src/app/components/base/Card/index.tsx
+++ src/app/components/base/Card/index.tsx
@@ -1,22 +1,24 @@
 import React, { PropsWithChildren } from "react";
-import { StyleProp, TextStyle } from "react-native";
+import { GestureResponderEvent, StyleProp, ViewStyle } from "react-native";
 import { useTheme } from "styled-components";
 import { Container } from "./styles";
 
 interface ICardProps extends PropsWithChildren {
-  style?: StyleProp<TextStyle>;
-  onPress?: (value?: string | number) => void;
+  style?: StyleProp<ViewStyle>;
+  onPress?: (e: GestureResponderEvent) => void;
 }
 
 export const Card = ({ children, onPress, style }: ICardProps) => {
   const { colors } = useTheme();
 
+  const styles: ViewStyle = {
+    backgroundColor: colors.PRIMARY,
+    borderRadius: 8,
+    ...(style as object),
+  };
+
   return (
-    <Container
-      mode="contained"
-      style={{ backgroundColor: colors.PRIMARY_INACTIVE }}
-      onPress={onPress && onPress}
-    >
+    <Container style={styles} onPress={onPress && onPress}>
       {children}
     </Container>
   );
--- src/app/components/base/Card/styles.ts
+++ src/app/components/base/Card/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components/native";
 import { Card } from "react-native-paper";
 
-export const Container = styled(Card)`
+export const Container = styled.TouchableOpacity`
   padding: 16px;
 `;
