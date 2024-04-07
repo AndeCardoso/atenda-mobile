0bb66b2c0d6b113bf443382d3ab069e7a33e2afc
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-04-06 22:56:13 -0300

    feat: Add model types to technician and address

---
+++ src/model/entities/address.ts
@@ -0,0 +1,10 @@
+export interface IAdressModel {
+  nickname?: string;
+  cep: string;
+  street: string;
+  number: string;
+  complement?: string;
+  district: string;
+  state: string;
+  city: string;
+}
