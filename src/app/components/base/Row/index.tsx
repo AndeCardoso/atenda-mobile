da01525a65ee48092e74e671b1c9bf384aa0706f
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-04-06 22:08:20 -0300

    update: Add space type prop

--- src/app/components/base/Row/index.tsx
+++ src/app/components/base/Row/index.tsx
@@ -1,14 +1,18 @@
 import React, { PropsWithChildren } from "react";
-import { View, ViewProps } from "react-native";
+import { View, ViewProps, ViewStyle } from "react-native";
 import { StyledRow } from "./styles";
 
 interface IRowProps extends ViewProps {
   gap?: number;
+  space?: "space-between" | "space-around" | "space-evenly";
 }
 
-export const Row = ({ children, gap, ...rest }: IRowProps) => {
+export const Row = ({ children, gap, space, ...rest }: IRowProps) => {
+  const styles: ViewStyle = {
+    justifyContent: space,
+  };
   return (
-    <StyledRow gap={gap} {...rest}>
+    <StyledRow gap={gap} style={styles} {...rest}>
       {children}
     </StyledRow>
   );
