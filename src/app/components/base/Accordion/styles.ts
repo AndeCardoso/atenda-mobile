50c78e7caf5065e44857c833d2d0b016f67f8b89
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-04-17 19:20:39 -0300

    feat: Add accordion component

---
+++ src/app/components/base/Accordion/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+export const Content = styled.View`
+  top: -8px;
+  padding: 18px;
+  padding-bottom: 0;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.SECONDARY_INACTIVE};
+`;
