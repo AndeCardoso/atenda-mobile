486d3877326d98a89da0bf775ac24b2f252a07bd
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-04-17 19:21:21 -0300

    feat: Add customer card component

---
+++ src/app/components/cards/CustomerCard/index.tsx
@@ -0,0 +1,69 @@
+import { Text } from "@components/base/Text";
+import React from "react";
+import { Card } from "@components/base/Card";
+import { Row } from "@components/base/Row";
+import { Chip } from "@components/base/Chip";
+import { Divider } from "@components/base/Separator";
+import { DisplayField } from "@components/base/DisplayField";
+import { formatCellphoneNumber, formatCpf } from "@utils/formatString";
+import { customerStatusDisplay } from "@pages/signedIn/customers/constants";
+import { BottomContainer, CardContainer } from "./styles";
+import { GestureResponderEvent } from "react-native";
+import { useTheme } from "styled-components";
+import { ICustomerModel } from "@model/entities/customer";
+
+interface ICustomerCardProps {
+  data: Partial<ICustomerModel>;
+  onPress?: (e: GestureResponderEvent) => void;
+}
+
+export const CustomerCard = ({
+  data: { name, document, phone, email, status },
+  onPress,
+}: ICustomerCardProps) => {
+  const { colors } = useTheme();
+  return (
+    <Card
+      onPress={onPress && onPress}
+      style={{ backgroundColor: colors.SECONDARY_INACTIVE }}
+    >
+      <CardContainer>
+        <Row space="space-between">
+          <Text
+            color="WHITE"
+            size={24}
+            weight="700"
+            numberOfLines={1}
+            fullwidth
+          >
+            {name}
+          </Text>
+
+          {status ? (
+            <Chip
+              text={customerStatusDisplay[status]}
+              color="PRIMARY"
+              textColor="SECONDARY"
+            />
+          ) : null}
+        </Row>
+        <Divider color="WHITE" />
+        <BottomContainer>
+          <DisplayField
+            text="Documento"
+            value={formatCpf(document)}
+            color="WHITE"
+          />
+          <Row>
+            <DisplayField text="E-mail" value={email} color="WHITE" />
+            <DisplayField
+              text="Celular"
+              value={formatCellphoneNumber(phone)}
+              color="WHITE"
+            />
+          </Row>
+        </BottomContainer>
+      </CardContainer>
+    </Card>
+  );
+};
