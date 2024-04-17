78c0c05eada85ce3bdf90f6fd122156a926fed60
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-04-17 19:21:00 -0300

    update: Adjustment of row component

--- src/app/components/base/Row/index.tsx
+++ src/app/components/base/Row/index.tsx
@@ -5,11 +5,20 @@ import { StyledRow } from "./styles";
 interface IRowProps extends ViewProps {
   gap?: number;
   space?: "space-between" | "space-around" | "space-evenly";
+  widthType?: "auto" | "half" | "full";
 }
 
-export const Row = ({ children, gap, space, ...rest }: IRowProps) => {
+export const Row = ({
+  children,
+  gap,
+  space,
+  widthType = "full",
+  ...rest
+}: IRowProps) => {
   const styles: ViewStyle = {
     justifyContent: space,
+    width:
+      widthType === "half" ? "50%" : widthType === "auto" ? "auto" : "100%",
   };
   return (
     <StyledRow gap={gap} style={styles} {...rest}>
