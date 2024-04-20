c08a77fa875dc8c6f67ebe5dda350f5539eb14f1
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-04-20 11:26:04 -0300

    update: Add a address list item component to customer form pages

---
+++ src/app/pages/signedIn/customers/components/AddressListItem/index.tsx
@@ -0,0 +1,49 @@
+import { Card } from "@components/base/Card";
+import { IconButton } from "@components/base/IconButton";
+import { Row } from "@components/base/Row";
+import { Text } from "@components/base/Text";
+import { IAddressForm } from "@components/forms/AddressForm/formSchema";
+import React from "react";
+
+interface IAddressListItem {
+  index: number;
+  data: Partial<IAddressForm>;
+  isLast: boolean;
+  isEditing: boolean;
+  handleEditAddressByIndex: (value: number) => void;
+  handleDecreaseAddressByIndex: (value: number) => void;
+}
+
+export const AddressListItem = ({
+  data,
+  index,
+  isLast,
+  isEditing,
+  handleEditAddressByIndex,
+  handleDecreaseAddressByIndex,
+}: IAddressListItem) => {
+  return (
+    <Card>
+      <Row space="space-between">
+        <Text color="BLACK" size={16} weight="600">
+          {data.nickname}
+        </Text>
+        <Row widthType="auto" gap={4}>
+          <IconButton
+            name="file-edit"
+            onPress={() => handleEditAddressByIndex(index)}
+          />
+          {isEditing && !isLast ? (
+            <>
+              <Text size={18}>|</Text>
+              <IconButton
+                name="close-box"
+                onPress={() => handleDecreaseAddressByIndex(index)}
+              />
+            </>
+          ) : null}
+        </Row>
+      </Row>
+    </Card>
+  );
+};
