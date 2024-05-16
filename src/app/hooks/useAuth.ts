6cf2469c1b23de829263caa7ae1b0cf2675d0491
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-05-15 22:30:13 -0300

    fix: Adjustment of token keeping at async storage

--- src/app/hooks/useAuth.ts
+++ src/app/hooks/useAuth.ts
@@ -7,15 +7,11 @@ import { AsyncAuthEnum } from "@model/asyncStorage/auth";
 import { useAuthContext } from "@contexts/auth/useAuthContext";
 
 export const useAuth = () => {
-  const { navigate } = useNavigation<any>();
   const { resetAuthStates, userData } = useAuthContext();
 
   const logout = async () => {
     await AsyncStorage.removeItem(AsyncAuthEnum.TOKEN);
     resetAuthStates();
-    navigate(Navigators.SIGNED_OFF_NAVIGATOR, {
-      screen: SignedOffScreens.AUTH,
-    });
   };
 
   return {
