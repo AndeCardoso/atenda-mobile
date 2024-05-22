02c0af40c8e964031d37924ea3ed842a6e48c602
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-05-21 21:28:53 -0300

    update: Add icon prop to baloon component

--- src/app/components/base/BaloonCard/index.tsx
+++ src/app/components/base/BaloonCard/index.tsx
@@ -1,29 +1,45 @@
 import React, { PropsWithChildren } from "react";
 import { Surface } from "react-native-paper";
-import { Colors } from "../Text";
 import { ViewStyle } from "react-native";
 import { useTheme } from "styled-components";
 import { ElevationLevels } from "react-native-paper/lib/typescript/types";
+import { Colors } from "@global/styles/colors";
+import { Icon } from "../Icon";
+import { TIconNames } from "../Icon/types";
 
 interface IBaloonCardProps extends PropsWithChildren {
   color?: Colors;
   elevation?: ElevationLevels;
+  styles?: ViewStyle;
+  icon?: {
+    name: TIconNames;
+    color: Colors;
+    size: number;
+  };
 }
 
 export const BaloonCard = ({
   color,
   elevation = 4,
   children,
+  styles,
+  icon,
 }: IBaloonCardProps) => {
   const { colors } = useTheme();
   const style: ViewStyle = {
     width: "100%",
     padding: 16,
-    backgroundColor: colors[color],
+    flexDirection: "row",
+    backgroundColor: colors[color ?? "PRIMARY"],
     borderRadius: 8,
+    gap: 8,
+    ...styles,
   };
   return (
     <Surface style={style} elevation={elevation}>
+      {icon ? (
+        <Icon name={icon.name} size={icon.size} color={icon.color} />
+      ) : null}
       {children}
     </Surface>
   );
