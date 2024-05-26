2fdb80b6ac14098b378a7d3e850001b9a63ea5bd
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-05-25 22:57:00 -0300

    update: Add padding prop to loader component

--- src/app/components/base/Loader/index.tsx
+++ src/app/components/base/Loader/index.tsx
@@ -7,16 +7,24 @@ import { Colors } from "@global/styles/colors";
 interface ILoaderProps {
   size?: number | "small" | "large";
   color?: Colors;
+  padding?: number;
   style?: ViewStyle;
 }
 
-export const Loader = ({ size, color, style }: ILoaderProps) => {
+export const Loader = ({
+  size,
+  color = "PRIMARY",
+  padding = 0,
+  style,
+}: ILoaderProps) => {
   const { colors } = useTheme();
+
+  const styleLoader = {
+    ...style,
+    padding: padding,
+  };
+
   return (
-    <ActivityIndicator
-      size={size}
-      color={colors[color ?? "PRIMARY"]}
-      style={style}
-    />
+    <ActivityIndicator size={size} color={colors[color]} style={styleLoader} />
   );
 };
